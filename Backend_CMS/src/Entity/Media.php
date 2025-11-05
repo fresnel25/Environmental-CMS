42854169bdd970eba333abf0d983e149e3d88b21
fresnel25 <ngaleufresnel@gmail.com>
2025-11-05 14:58:22 +0100

    mise en place du bundle Api

--- Backend_CMS/src/Entity/Media.php
+++ Backend_CMS/src/Entity/Media.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\MediaRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: MediaRepository::class)]
+#[ApiResource()]
 class Media
 {
     #[ORM\Id]
--- Backend_CMS/src/Entity/Theme.php
+++ Backend_CMS/src/Entity/Theme.php
@@ -2,11 +2,13 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\ThemeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ThemeRepository::class)]
+#[ApiResource()]
 class Theme
 {
     #[ORM\Id]
